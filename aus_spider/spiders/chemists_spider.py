3b3cb5952dc2a6e0fefa91f11e1316896787f4dc
s65b40 <haochunwang@hotmail.com>
2018-04-04 23:09:38 +1000

    v1.0
    v1.0 finish most necessary functions

--- aus_spider/spiders/chemists_spider.py
+++ aus_spider/spiders/chemists_spider.py
@@ -6,14 +6,16 @@ import re
 from scrapy import *
 import re, sys
 import requests
+
 if sys.getdefaultencoding() != 'utf-8':
     reload(sys)
     sys.setdefaultencoding('utf-8')
-url = 'http://www.boc.cn/sourcedb/whpj/index.html'  # 网址
-html = requests.get(url).content.decode('utf8')  # 获取网页源码（中间涉及到编码问题,这是个大坑，你得自己摸索）
-a = html.index('<td>澳大利亚元</td>')  # 取得“新西兰元”当前位置
-s = html[a:a + 300]  # 截取新西兰元汇率那部分内容（从a到a+300位置）
-rateres = float(re.findall('<td>(.*?)</td>', s)[3]) * 0.01  # 正则获取
+url = 'http://www.boc.cn/sourcedb/whpj/index.html'  # Bank of China currency website
+html = requests.get(url).content.decode('utf8')
+a = html.index('<td>澳大利亚元</td>')  # get the position of AUS dollar
+s = html[a:a + 300]  # narrow down the range
+rate_res = float(re.findall('<td>(.*?)</td>', s)[3]) * 0.01  # Regex get the currency
+
 
 class ChemistSpider(scrapy.Spider):
     name = "chemistwarehouse"
@@ -24,6 +26,7 @@ class ChemistSpider(scrapy.Spider):
         "http://www.chemistwarehouse.com.au/Shop-Online/587/Swisse?page=3",
         "http://www.chemistwarehouse.com.au/Shop-Online/587/Swisse?page=4",
         "http://www.chemistwarehouse.com.au/Shop-Online/587/Swisse?page=5",
+
         "http://www.chemistwarehouse.com.au/Shop-Online/513/Blackmores",
         "http://www.chemistwarehouse.com.au/Shop-Online/513/Blackmores?page=2",
         "http://www.chemistwarehouse.com.au/Shop-Online/513/Blackmores?page=3",
@@ -31,37 +34,61 @@ class ChemistSpider(scrapy.Spider):
         "http://www.chemistwarehouse.com.au/Shop-Online/513/Blackmores?page=5",
         "http://www.chemistwarehouse.com.au/Shop-Online/513/Blackmores?page=6",
         "http://www.chemistwarehouse.com.au/Shop-Online/513/Blackmores?page=7",
+
         "http://www.chemistwarehouse.com.au/Shop-Online/660/Nature-s-Way",
         "http://www.chemistwarehouse.com.au/Shop-Online/660/Nature-s-Way?page=2",
         "http://www.chemistwarehouse.com.au/Shop-Online/660/Nature-s-Way?page=3",
-        "http://www.chemistwarehouse.com.au/Shop-Online/660/Nature-s-Way?page=4"
+        "http://www.chemistwarehouse.com.au/Shop-Online/660/Nature-s-Way?page=4",
+
+        "http://www.chemistwarehouse.com.au/Shop-Online/722/Healthy-Care",
+        "http://www.chemistwarehouse.com.au/Shop-Online/722/Healthy-Care?page=2",
+        "http://www.chemistwarehouse.com.au/Shop-Online/722/Healthy-Care?page=3",
+        "http://www.chemistwarehouse.com.au/Shop-Online/722/Healthy-Care?page=4",
+        "http://www.chemistwarehouse.com.au/Shop-Online/722/Healthy-Care?page=5",
+
+        "http://www.chemistwarehouse.com.au/Shop-Online/2128/Bio-Island"
     ]
 
     def parse(self, response):
         selector1 = Selector(response)
-        namesp = selector1.xpath('//a[@class="product-container"]/@href').extract()
-        pricesp = selector1.xpath('//span[@class="Price"]').extract()
-
-        pricesplitlst = str(pricesp).split(',')
+        name_space = selector1.xpath('//a[@class="product-container"]/@href').extract()
+        price_space = selector1.xpath('//span[@class="Price"]').extract()
+        pricesv = selector1.xpath('//div[@class="prices"]').extract()
+        #print pricesv
+        price_split_list = str(price_space).split(',')
         #p1 = r"(?<=u\''/buy'/\d+/).+?(?=\')"
         #pattern1 = re.compile(p1)
         p2 = r"(?<=u\'<span class=\"Price\">).+?(?=</span>\')"
         pattern2 = re.compile(p2)
-        name_res_lst = []
-        price_res_lst = []
-        for i in namesp:
+        name_res_lst = []     # 商品名列表 product name list
+        price_res_lst = []    # 价格列表 price list
+        discount_res_lst = [] # 折扣列表 discount list
+        for i in name_space:
             #name = re.search(pattern1, i)
             #name_res_lst.append(name.group(0)[1:])
             name_res_lst.append(i[11:])
 
-        for j in pricesplitlst:
+        for j in price_split_list:
             price = re.search(pattern2, j)
             price_res_lst.append(price.group(0))
 
+        for i in pricesv:
+            u = i.split('class="Price">')
+            price_item = float(u[1].split('\n')[0][1:])
+            if 'class="Save"' in u[1]:
+                #print 'yes'
+                save_item = float(u[1].split('SAVE')[1].split('</span>')[0].strip(' ').strip('\n')[1:])
+                #print save_item
+                discount = '%.1f' % (price_item / (price_item + save_item) * 10)
+                #print discount
+                discount_res_lst.append(discount)
+            else:
+                discount_res_lst.append(10)
         for k in range(len(name_res_lst)):
             with open("res_tmp.txt", "a+") as b:
-                b.writelines(name_res_lst[k]+', '+price_res_lst[k].split(' ')[0] + ', ' +
-                             str((float(price_res_lst[k].split(' ')[0][1:]) + 10) * (rateres + 0.3)) +'\n')
+                b.writelines(name_res_lst[k] + ', '+price_res_lst[k].split(' ')[0] + ', ' +
+                             str((float(price_res_lst[k].split(' ')[0][1:]) + 10) * (rate_res + 0.3))
+                             + ', ' + str(discount_res_lst[k]) + '\n')
 
         #正则表达式必知必会
         #.extract()
--- rate.py
+++ rate.py
@@ -1,65 +1,86 @@
 # -*- coding: utf-8 -*-
 #author:Haochun Wang
 import xlrd, xlwt, os, time
+class Tablemaker:
+    def __init__(self):
+        self.font0 = xlwt.Font()
+        self.font0.colour_index = 2  # red
+        self.font1 = xlwt.Font()
+        self.font1.colour_index = 3  # green
+        self.style0 = xlwt.XFStyle()
+        self.style1 = xlwt.XFStyle()
+        self.style0.font = self.font0
+        self.style1.font = self.font1
+        self.write_table()
 
-font0 = xlwt.Font()
-font0.colour_index = 2
-font1 = xlwt.Font()
-font1.colour_index = 3
-style0 = xlwt.XFStyle()
-style1 = xlwt.XFStyle()
-style0.font = font0
-style1.font = font1
-with open('res_tmp.txt') as d:
-    p = d.readlines()
-    products = sorted(p)
+    def write_table(self):
+        with open('res_tmp.txt') as d:   # open the temporary text file
+            p = d.readlines()
+            products = sorted(p)   # sort the lines by the alphabet order of product names
+            timestamp = time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime(time.time()))
+            path_exist = 'price_folder/price.xls'
+            save_path = 'price_folder/price' + timestamp + '.xls'
+            path_folder = '/Users/hcwang/Desktop/spider-py/aus_spider/price_folder/'
 
-    path_exist = 'price.xls'
-    save_path = 'price.xls'
-    if os.path.exists(path_exist):
-        color = ['black', 'red']
-        oldsheet = xlrd.open_workbook(path_exist).sheet_by_index(0)
-        numrow = oldsheet.nrows
-        olddic = {}
-        for i in range(1, numrow):
-            olddic[oldsheet.row_values(i)[0].replace(' ', '-').replace('\'', '39')] = oldsheet.row_values(i)[1]
+            try:
+                lists = os.listdir(path_folder)  # 列出目录的下所有文件和文件夹保存到lists
+                lists.sort(key=lambda fn: os.path.getmtime(path_folder + "/" + fn))  # 按时间排序
+                path_exist = os.path.join(path_folder, lists[-1])  # 获取最新的文件保存到file_new
 
-        newbook = xlwt.Workbook(encoding='utf-8', style_compression=0)
-        newsheet = newbook.add_sheet('sheet1', cell_overwrite_ok=True)
+                oldsheet = xlrd.open_workbook(path_exist).sheet_by_index(0)
+                numrow = oldsheet.nrows
+                olddic = {}
+                for i in range(1, numrow):
+                    olddic[oldsheet.row_values(i)[0].replace(' ', '-').replace('\'', '39')] = oldsheet.row_values(i)[1]
 
-        currentrow = 1
-        newsheet.write(0, 0, 'product_name_en')
-        newsheet.write(0, 1, 'price_aud')
-        newsheet.write(0, 2, 'price_yuan')
-        for i in xrange(len(products) - 1):
-            rowsplits = products[i].split(', ')
-            #print rowsplits[1][1:]
-            #print type(rowsplits[1][1:])
-            #print len(rowsplits[1][1:])
-            newsheet.write(currentrow, 0, rowsplits[0].replace('-', ' ').replace('39', '\''))
-            if olddic.has_key(rowsplits[0]) and float(olddic[rowsplits[0]]) > float(rowsplits[1][1:]):
-                newsheet.write(currentrow, 1, float(rowsplits[1][1:]), style0)
-                newsheet.write(currentrow, 2, float(rowsplits[2]), style0)
-            elif olddic.has_key(rowsplits[0]) and float(olddic[rowsplits[0]]) < float(rowsplits[1][1:]):
-                newsheet.write(currentrow, 1, float(rowsplits[1][1:]), style1)
-                newsheet.write(currentrow, 2, float(rowsplits[2]), style1)
-            else:
-                newsheet.write(currentrow, 1, float(rowsplits[1][1:]))
-                newsheet.write(currentrow, 2, float(rowsplits[2]))
-            currentrow += 1
-        newbook.save(save_path)
-    else:
-        newbook = xlwt.Workbook(encoding='utf-8',style_compression=0)
-        newsheet = newbook.add_sheet('sheet1', cell_overwrite_ok=True)
+                newbook = xlwt.Workbook(encoding='utf-8', style_compression=0)
+                newsheet = newbook.add_sheet('sheet1', cell_overwrite_ok=True)
+                new_sheet_sorted = newbook.add_sheet('sheet2', cell_overwrite_ok=True)
 
-        currentrow = 1
-        newsheet.write(0, 0, 'product_name_en')
-        newsheet.write(0, 1, 'price_aud')
-        newsheet.write(0, 2, 'price_yuan')
-        for i in xrange(len(products)-1):
-            rowsplits = products[i].split(', ')
-            newsheet.write(currentrow, 0, rowsplits[0].replace('-', ' ').replace('39', '\''))
-            newsheet.write(currentrow, 1, float(rowsplits[1][1:]))
-            newsheet.write(currentrow, 2, float(rowsplits[2]))
-            currentrow += 1
-        newbook.save(save_path)
+                currentrow = 1
+                newsheet.write(0, 0, 'product_name_en')
+                newsheet.write(0, 1, 'price_aud')
+                newsheet.write(0, 2, 'price_yuan')
+                newsheet.write(0, 3, 'discount')      
+
+                for i in xrange(len(products) - 1):
+                    rowsplits = products[i].split(', ')
+                    #print rowsplits[1][1:]
+                    #print type(rowsplits[1][1:])
+                    #print len(rowsplits[1][1:])
+                    newsheet.write(currentrow, 0, rowsplits[0].replace('-', ' ').replace('39', '\''))
+                    if olddic.has_key(rowsplits[0]) and float(olddic[rowsplits[0]]) > float(rowsplits[1][1:]):
+                        newsheet.write(currentrow, 1, float(rowsplits[1][1:]), self.style0)
+                        newsheet.write(currentrow, 2, float(rowsplits[2]), self.style0)
+                        newsheet.write(currentrow, 3, float(rowsplits[3]), self.style0)
+                    elif olddic.has_key(rowsplits[0]) and float(olddic[rowsplits[0]]) < float(rowsplits[1][1:]):
+                        newsheet.write(currentrow, 1, float(rowsplits[1][1:]), self.style1)
+                        newsheet.write(currentrow, 2, float(rowsplits[2]), self.style1)
+                        newsheet.write(currentrow, 3, float(rowsplits[3]), self.style1)
+                    else:
+                        newsheet.write(currentrow, 1, float(rowsplits[1][1:]))
+                        newsheet.write(currentrow, 2, float(rowsplits[2]))
+                        newsheet.write(currentrow, 3, float(rowsplits[3]))
+                    currentrow += 1
+                newbook.save(save_path)
+            except:
+                newbook = xlwt.Workbook(encoding='utf-8',style_compression=0)
+                newsheet = newbook.add_sheet('sheet1', cell_overwrite_ok=True)
+
+                currentrow = 1
+                newsheet.write(0, 0, 'product_name_en')
+                newsheet.write(0, 1, 'price_aud')
+                newsheet.write(0, 2, 'price_yuan')
+                newsheet.write(0, 3, 'discount')
+                for i in xrange(len(products)-1):
+                    rowsplits = products[i].split(', ')
+                    newsheet.write(currentrow, 0, rowsplits[0].replace('-', ' ').replace('39', '\''))
+                    newsheet.write(currentrow, 1, float(rowsplits[1][1:]))
+                    newsheet.write(currentrow, 2, float(rowsplits[2]))
+                    newsheet.write(currentrow, 3, float(rowsplits[3]))
+                    currentrow += 1
+                newbook.save(save_path)
+
+
+        return
+Tablemaker()
